d0e582a926cf8012b4c92a607f9bfba8daf9feb1
Georger Araújo <4930759+georgeraraujo@users.noreply.github.com>
2022-11-11 16:39:57 -0300

    More specific loading of the localized tutorial (#285)
    * More specific loading of the localized tutorial
    
    First try and load the file ending with the five-letter canonical form of the current locale name, e.g. `tutorial-pt_BR.cts`.
    Failing that, try and load the file ending with the two-letter canonical form of the current locale name, e.g. `tutorial-pt.cts`.
    Failing that, just load `tutorial.cts`, and be done with it.
    
    * Make `LoadTut()` work when `lang` is empty

--- src/system.h
+++ src/system.h
@@ -208,10 +208,17 @@ struct System {
 
     void LoadTut() {
         auto lang = frame->app->locale.GetCanonicalName();
-        lang.Truncate(2);
-        if (LoadDB(frame->GetDocPath(L"examples/tutorial-" + lang + ".cts"))[0]) {
-            LoadDB(frame->GetDocPath(L"examples/tutorial.cts"));
+
+        if (lang.Len() == 5 && !LoadDB(frame->GetDocPath(L"examples/tutorial-" + lang + ".cts"))[0]) {
+            return;
         }
+
+        lang.Truncate(2);
+        if (lang.Len() == 2 && !LoadDB(frame->GetDocPath(L"examples/tutorial-" + lang + ".cts"))[0]) {
+            return;
+        }
+
+        LoadDB(frame->GetDocPath(L"examples/tutorial.cts"));
     }
 
     void LoadOpRef() { LoadDB(frame->GetDocPath(L"examples/operation-reference.cts")); }
