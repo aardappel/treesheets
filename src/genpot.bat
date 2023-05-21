6497ed62d4b06915ea1063597cddc98ead7c5483
Georger Araújo <4930759+georgeraraujo@users.noreply.github.com>
2023-05-21 15:40:41 -0300

    Mark strings for translation & update pt_BR

--- src/genpot.bat
+++ src/genpot.bat
@@ -1,1 +1,1 @@
-xgettext --keyword=_ --sort-output --no-location -o ../TS/translations/ts.pot myframe.h document.h system.h
+xgettext --keyword=_ --sort-output --no-location -o ../TS/translations/ts.pot document.h myapp.h myframe.h system.h
