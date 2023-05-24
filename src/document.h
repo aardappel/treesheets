6d8cb8174469a2ae32d6aad1c79a6ed48f353e66
Tobias Predel <tobias.predel@gmail.com>
2023-05-24 08:47:50 +0200

    Add (almost) whole day of end date
    If not, DateTime is set to <date>T00:00:00
    which is a little bit counterintuitive.
    
    Also remove extra check because input
    validation is already done by the control element
    itself.

--- src/document.h
+++ src/document.h
@@ -1219,12 +1219,8 @@ struct Document {
                     return nullptr;
                 }
                 wxDateTime beginrange = start->GetValue();
-                wxDateTime endrange = end->GetValue();
-                if(beginrange.IsValid() && endrange.IsValid()) {
-                    ApplyEditRangeFilter(beginrange, endrange);
-                } else {
-                    return _(L"No valid datetime given!");
-                }
+                wxDateTime endrange = end->GetValue().Add(wxTimeSpan(23, 59, 59, 999));
+                ApplyEditRangeFilter(beginrange, endrange);
                 return nullptr;
             }
 
