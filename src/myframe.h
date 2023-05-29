e524d62cd5a8221e4c0478ef8e3fc397fc3019a8
Tobias Predel <tobias.predel@gmail.com>
2023-05-29 11:16:06 +0200

    Remove extraneous no-c-format declaration
    This does not apply to the corresponding item.

--- src/myframe.h
+++ src/myframe.h
@@ -461,7 +461,6 @@ struct MyFrame : wxFrame {
         wxMenu *filtermenu = new wxMenu();
         MyAppend(filtermenu, A_FILTEROFF, _(L"Turn filter &off"));
         MyAppend(filtermenu, A_FILTERS, _(L"Show only cells in current search"));
-        // xgettext:no-c-format
         MyAppend(filtermenu, A_FILTERRANGE, _(L"Show last edits in specific date range"));
         // xgettext:no-c-format
         MyAppend(filtermenu, A_FILTER5, _(L"Show 5% of last edits"));
