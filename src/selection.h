3a68d790863f53ec32e6314d4dd19c850cb577bf
AntonBogun <bogunanton@gmail.com>
2023-08-08 13:00:05 +0300

    fix formatting

--- src/selection.h
+++ src/selection.h
@@ -131,20 +131,20 @@ class Selection {
                 wxChar ch;
                 if (c >= 0 && c <= MaxCursor()) {
                     ch = GetCell()->text.t[min(c, curs)];
-                    //TEXT_SPACE > TEXT_SEP > TEXT_CHAR > 0.
-                    //Accepts smaller or equal type when positive, only equal when negative.
-                    //in regex terms (space/sep/char = s/p/c): match (s+p*|s+c*|p+c*|c+)
+                    // TEXT_SPACE > TEXT_SEP > TEXT_CHAR > 0.
+                    // Accepts smaller or equal type when positive, only equal when negative.
+                    // in regex terms (space/sep/char = s/p/c): match (s+p*|s+c*|p+c*|c+)
                     int allowed = CharType(ch);
                     curs = c;
-                    while(true) {
+                    for (;;) {
                         c += dx;
                         if (c < 0 || c > MaxCursor()) break;
                         ch = GetCell()->text.t[min(c,curs)];
                         int chtype = CharType(ch);
-                        //type increase when positive or type change when negative => break
+                        // type increase when positive or type change when negative => break
                         if (chtype > allowed && chtype != -allowed ) break;
                         curs = c;
-                        //type decrease when positive => negate
+                        // type decrease when positive => negate
                         if (chtype < allowed) allowed = -chtype;
                     }
                 }
