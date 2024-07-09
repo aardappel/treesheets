1d83e1846a47bc53707b4f13dc73d5779b53991a
Tobias Predel <tobias.predel@gmail.com>
2024-07-09 11:27:01 +0200

    Update batch file to generate script reference
    Lobster now puts the dump of builtins into the scripts folder

--- TS/docs/script_refererence_update.bat
+++ TS/docs/script_refererence_update.bat
@@ -1,5 +1,5 @@
 rem NOTE: this uses the Release mode executable!
 ..\TreeSheets.exe -d
 del script_reference.html
-ren builtin_functions_reference.html script_reference.html
+move ..\scripts\builtin_functions_reference.html script_reference.html
 pause
