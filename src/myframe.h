d7f731d8076df456215dd58e5b8231bdb9a6e4b9
Tobias Predel <tobias.predel@gmail.com>
2025-03-31 23:46:39 +0200

    Reassign translatable strings to match behavior

--- src/myframe.h
+++ src/myframe.h
@@ -327,7 +327,8 @@ struct MyFrame : wxFrame {
             MyAppend(temenu, A_CEND, _(L"End of text") + "\tCTRL+END");
             temenu->AppendSeparator();
             MyAppend(temenu, A_ENTERCELL, _(L"Enter/exit text edit mode") + "\tENTER");
-            MyAppend(temenu, A_ENTERCELL_JUMPTOEND, _(L"Enter/exit text edit mode") + "\tF2");
+            MyAppend(temenu, A_ENTERCELL_JUMPTOEND,
+                     _(L"Enter/exit text edit to the right") + "\tF2");
             MyAppend(temenu, A_PROGRESSCELL, _(L"Enter/exit text edit to the right") + "\t" CTRLORALT "+ENTER");
             MyAppend(temenu, A_CANCELEDIT, _(L"Cancel text edits") + "\tESC");
 
