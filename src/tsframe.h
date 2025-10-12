d0d7230eb9d9e5380cb940fa2b91c2f395c3ef83
Tobias Predel <tobias.predel@gmail.com>
2025-10-12 09:33:46 +0200

    Remove cast

--- src/tsframe.h
+++ src/tsframe.h
@@ -863,7 +863,7 @@ struct TSFrame : wxFrame {
     }
 
     void CycleTabs(int offset = 1) {
-        auto numtabs = static_cast<int>(notebook->GetPageCount());
+        auto numtabs = notebook->GetPageCount();
         offset = ((offset >= 0) ? 1 : numtabs - 1);  // normalize to non-negative wrt modulo
         notebook->SetSelection((notebook->GetSelection() + offset) % numtabs);
     }
