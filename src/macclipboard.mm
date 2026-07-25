e9e0b5f211df704b1d1b57b826069bd0e4df06c0
Tobias Predel <tobias.predel@gmail.com>
2026-07-25 07:14:24 +0200

    Try to load PNG or JPEG first

--- src/macclipboard.mm
+++ src/macclipboard.mm
@@ -3,41 +3,36 @@
 MacClipboardResult GetImageFromMacClipboard() {
   @autoreleasepool {
     NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
-    NSArray *classes = @[ [NSImage class] ];
-    NSDictionary *options = @{};
 
-    if (![pasteboard canReadObjectForClasses:classes options:options]) {
-      return {wxNullImage, 1.0};
-    }
+    // Check for direct image data types first
+    NSArray *supportedTypes = @[ NSPasteboardTypePNG, @"public.jpeg", NSPasteboardTypeTIFF ];
+    NSString *bestType = [pasteboard availableTypeFromArray:supportedTypes];
 
-    NSArray *copiedItems = [pasteboard readObjectsForClasses:classes options:options];
-    if (copiedItems.count == 0) {
-      return {wxNullImage, 1.0};
-    }
-
-    NSImage *nsImage = copiedItems.firstObject;
-    if (!nsImage) return {wxNullImage, 1.0};
-
-    double scaleFactor = 1.0;
-    NSSize pointSize = [nsImage size];
-
-    for (NSImageRep *rep in [nsImage representations]) {
-      if ([rep isKindOfClass:[NSBitmapImageRep class]]) {
-        NSInteger pixelWidth = [rep pixelsWide];
-        if (pointSize.width > 0 && pixelWidth > 0) {
-          scaleFactor = (double)pixelWidth / pointSize.width;
+    if (bestType) {
+      NSData *data = [pasteboard dataForType:bestType];
+      if (data && data.length > 0) {
+        double scaleFactor = 1.0;
+        NSArray *items = [pasteboard readObjectsForClasses:@[ [NSImage class] ] options:@{}];
+        if (items.count > 0) {
+          NSImage *nsImage = items.firstObject;
+          NSSize pointSize = [nsImage size];
+          for (NSImageRep *rep in [nsImage representations]) {
+            if ([rep isKindOfClass:[NSBitmapImageRep class]]) {
+              NSInteger pixelWidth = [rep pixelsWide];
+              if (pointSize.width > 0 && pixelWidth > 0) {
+                scaleFactor = (double)pixelWidth / pointSize.width;
+              }
+              break;
+            }
+          }
         }
-        break;
+
+        wxMemoryInputStream stream([data bytes], [data length]);
+        wxImage wxImg;
+        if (wxImg.LoadFile(stream, wxBITMAP_TYPE_ANY)) {
+          return {wxImg, scaleFactor};
+        }
       }
-    }
-
-    NSData *tiffData = [nsImage TIFFRepresentation];
-    if (!tiffData) return {wxNullImage, 1.0};
-
-    wxMemoryInputStream stream([tiffData bytes], [tiffData length]);
-    wxImage wxImg;
-    if (wxImg.LoadFile(stream, wxBITMAP_TYPE_ANY)) {
-      return {wxImg, scaleFactor};
     }
 
     return {wxNullImage, 1.0};
