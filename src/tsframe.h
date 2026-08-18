e013d8d36df3b15dafa9d92a4e0772bdc7142e77
Wouter van Oortmerssen <aardappel@gmail.com>
2026-08-18 14:50:58 -0700

    Do not cycle tabs when there are none
    The next tab is picked with a modulo of the number of tabs, which divides by
    zero if all of them are gone.

--- src/tsframe.h
+++ src/tsframe.h
@@ -1540,6 +1540,7 @@ struct TSFrame : wxFrame {
 
     void CycleTabs(int offset = 1) const {
         auto numtabs = static_cast<int>(notebook->GetPageCount());
+        if (numtabs == 0) { return; }
         offset = offset >= 0 ? 1 : numtabs - 1;  // normalize to non-negative wrt modulo
         notebook->SetSelection((notebook->GetSelection() + offset) % numtabs);
     }
